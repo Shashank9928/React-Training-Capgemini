54e5408209f5cda47fc7721355411db7b59ab2ec
Shashank Mathur <mathurshashank2299@gmail.com>
2022-04-12 14:13:51 +0530

    boostrap and common css

--- Projects/employee_managment_system/src/App.js
+++ Projects/employee_managment_system/src/App.js
@@ -1,18 +1,13 @@
 import React from "react";
 import RaiseComplain from "./Pages/Complience/RaiseComplain";
-import AllComplains from "./Pages/Complience/AllComplains";
-import ComplainByUserId from "./Pages/Complience/UserComplain";
-import AddStatusReport from "./Pages/Complience/AddStatusReport";
-import GetAllStatusReport from "./Pages/Complience/GetAllStatusReport";
+import Complains from "./Pages/Complience/Complains";
+// import AddStatusReport from "./Pages/Complience/AddStatusReport";
+// import GetAllStatusReport from "./Pages/Complience/GetAllStatusReport";
 function App() {
   return (
     <div className="App">
-
       <RaiseComplain />
-      <AllComplains />
-      <ComplainByUserId />
-      <AddStatusReport />
-      <GetAllStatusReport />
+      < Complains />    
     </div>
   );
 }
